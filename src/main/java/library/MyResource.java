f1ee0cd1af0d6c43854fd7c45f7141d2be57b291
gabrielr <gabrielr@HI96567.cegekanv.corp.local>
2016-09-06 18:06:21 +0300

    Initial commit

---
+++ src/main/java/library/MyResource.java
@@ -0,0 +1,26 @@
+package library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("books")
+public class MyResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<String> getBooks() {
+    	List<String> books = new ArrayList<>();
+    	
+    	books.add("Harap Alb");
+    	books.add("Harry Potter and the Philosopher's Stone");
+    	
+    	return books;
+    }
+}
